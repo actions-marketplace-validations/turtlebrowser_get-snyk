7b66a65e2fb58752a9ded1929bbb24e96d46ae90
Patricia Aas <psmaas@gmail.com>
2020-12-05 20:16:49 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,4 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 scoop bucket add snyk https://github.com/snyk/scoop-snyk
 scoop install snyk
+snyk version
